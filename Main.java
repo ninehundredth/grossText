be2da4d45b0ff4ac491d1b4b462bf46974c568f0
Marty <ninehundredth@gmail.com>
2018-04-24 18:45:01 +0300

    Added new class

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
-public class Main {
+public class Main extends PatternBuilder{
 
     public static void main(String[] args) {
         // Request for data input in console
@@ -14,49 +13,63 @@ public class Main {
         //Declare a var with the word length
         int charNumber = inputWord.length();
 
-        //Declare chars from the word has been input letter by letter.
-        char letter = inputWord.charAt(0);
+//        char letter = inputWord.charAt(0);
+
+        ArrayList<char[][]> toGrossLetters = new ArrayList<>();
 
 
-        char [][] aChar = new char[5][9];
-        int [][] a = new int[][]{
-                {0,0,0,0,1,0,0,0,0},
-                {0,0,0,1,0,1,0,0,0},
-                {0,0,1,0,0,0,1,0,0},
-                {0,1,1,1,1,1,1,1,0},
-                {1,0,0,0,0,0,0,0,1}
-        };
-        int [][] b = new int[][]{
-                {0,0,1,1,1,1,0,0,0},
-                {0,0,1,0,0,0,1,0,0},
-                {0,0,1,1,1,1,0,0,0},
-                {0,0,1,0,0,0,1,0,0},
-                {0,0,1,1,1,1,0,0,0}
+        for (char c : inputWord.toCharArray()) {
+            toGrossLetters.add(Main.getPatternForLetter(c));
+        }
+
+        //Cycle to convert letter binary pattern into a B letter pattern
+
+        //int row = 0;
+        //int col = 0;
+/* TEST ARRAY
+        int[][] array = new int[][]{{1, 2},
+                {3, 4}
         };
 
-        //Cycle to convert letter binary pattern into an A letter pattern
-        int row, col;
-        for (col = 0; col <9; col++){
-            for (row = 0; row < 5; row++){
-                if (a[row][col] == 1){
-                    aChar[row][col] = 'A';
-                }else{
-                    aChar[row][col] = ' ';
+        for (; row < 2; row++) {
+            for (int col=0; col < 2; col++) {
+                System.out.println(array[row][col]);
+            }
+        }
+*/
+        char[] toPrint = new char[((charNumber) * 9*5)];
+        int n = 0;
+        for (char[][] toLetter : toGrossLetters) {
+            for (int col = 0; col < 9; col++) {
+            for (int row = 0; row < 5; row++) {
+
+                    toPrint[n] = toLetter[row][col];
+                    n++;
                 }
             }
         }
+        int i = 0;
+
+        for (char l : toPrint) {
+            System.out.print(l);
+            i++;
+
+
+            //if
+        }
 
         //Cycle that prints the output of the A letter pattern
-        for (row = 0; row < 5; row++){
-            for (col = 0; col < 9; col++){
-
-                System.out.print(aChar[row][col]);
-            }
-            System.out.println("\n");
-        }
+//        for (row = 0; row < 5; row++){
+//            for (col = 0; col < 9; col++){
+//
+//                System.out.print(bChar[row][col]);
+//            }
+//            System.out.println();
+//        }
         //TEST
         /*int symbolNumber = 97;
         char symbolValue = (char) symbolNumber;
         System.out.println(symbolValue);*/
+
     }
 }
